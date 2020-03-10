f3d93b45f874d04d78344c483a149cb32871fb59
Marcin Sokołowski <mysy2001@interia.pl>
2020-03-10 21:29:28 +0100

    added test for checking isbn number length

--- src/test/java/com/mysy/isbn/validator/ValidateISBNTest.java
+++ src/test/java/com/mysy/isbn/validator/ValidateISBNTest.java
@@ -1,6 +1,7 @@
 package com.mysy.isbn.validator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,14 @@ class ValidateISBNTest {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkIsbn("0321125216");
         assertThat(result).isFalse();
+    }
+
+    @Test
+    void nineDigitIsbnIsNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+
+        assertThatThrownBy(() -> {
+            validator.checkIsbn("321125216");
+        }).isInstanceOf(NumberFormatException.class);
     }
 }
