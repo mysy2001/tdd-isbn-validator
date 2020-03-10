3c999a931a448f71f9bd628bb20ae365b0f495c1
Marcin Sokołowski <mysy2001@interia.pl>
2020-03-10 21:32:39 +0100

    added implementation of checking isbn number length

--- src/test/java/com/mysy/isbn/validator/ValidateISBN.java
+++ src/test/java/com/mysy/isbn/validator/ValidateISBN.java
@@ -2,6 +2,11 @@ package com.mysy.isbn.validator;
 
 public class ValidateISBN {
     public boolean checkIsbn(final String isbn) {
+
+        if (isbn.length() != 10) {
+            throw new NumberFormatException("Isbn number must be 10 digits long");
+        }
+
         int total = 0;
         for (int i = 0; i < 10; i++) {
             total += isbn.charAt(i) * (10 - i);
